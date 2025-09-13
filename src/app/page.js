86df94c0d2a1e0bc94ff36844e510f9c0f55d3a8
warnasweb <13366914+warnasweb@users.noreply.github.com>
2025-09-13 21:53:14 +1000

    raj changes

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,8 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            This has been changed to use the <code>app</code> directory and{" Warna"} 
+      
           </li>
         </ol>
 
